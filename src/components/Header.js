644b7b85cf0c4084bcc0ec95bad89b08b0262534
root <root@MayankJoshi>
2023-11-30 10:45:30 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Flex, Spacer, Button } from '@chakra-ui/react';
+import { Icon } from '../icons/Icons';
+
+function Header() {
+  return (
+    <Flex>
+      <Icon/>
+       
+    </Flex>
+  )
+}
+
+export default Header
