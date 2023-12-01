94d00dc24c92f77404d1dad43cad61ad372760b6
root <root@MayankJoshi>
2023-12-01 11:01:26 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Home from './pages/Home';
 import Header from './components/Header';
 import SideNav from './components/Navigation/SideNav';
+import Survey from './pages/Survey';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
@@ -9,7 +11,11 @@ function App() {
     <div>
       <Header/>
       <SideNav/>
-      {/* <Home/> */}
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='survey' element={<Survey/>}/>
+      </Routes>
+      
     </div>
   )
 }
